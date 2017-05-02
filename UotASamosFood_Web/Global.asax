7f4fcda42820842d479e8020dda3188eb109ea8c
George Makrakis <George Makrakis>
2017-05-02 23:00:54 +0300

    initial commit

---
+++ UotASamosFood_Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UotASamosFood_Web.MvcApplication" Language="C#" %>
---
+++ UotASamosFood_Web/Models/FoodClass.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UotASamosFood_Web.Models
+{
+    public class FoodClass
+    {
+        [Display(Name = "Week")]
+        public string week_sel { get; set; }
+
+        [Display(Name = "Day Time")]
+        public string daytime_sel { get; set; }
+    }
+}
